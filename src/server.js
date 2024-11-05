6c4d6f16273fbb74e4f9e69722a8c49c558e9cd1
Drmobkey <yunantobiantoro8@gmail.com>
2024-11-05 13:53:08 +0700

    joll

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: 8001,
     host: 'localhost',
     routes: {
-      cors: ['*'],
+      cors: {
+        origin: ['*'], // Atur CORS untuk menerima dari semua asal
+      },
     },
   });
   
